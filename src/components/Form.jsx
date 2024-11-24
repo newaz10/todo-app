745a2aba4ed4f3f9711080c45d0c32785379c0eb
newaz10 <mh.newaz001@gmail.com>
2024-11-24 22:45:22 +0600

    styling the form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,5 +1,7 @@
 import PropTypes from "prop-types";
 import { useState } from "react";
+import "./Form.css";
+
 export const Form = ({ setTodoList }) => {
   const [todo, setTodo] = useState("");
 
@@ -19,14 +21,19 @@ export const Form = ({ setTodoList }) => {
     }
   };
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        value={todo}
-        onChange={handleChange}
-        placeholder="Enter your task"
-      />
-      <button type="submit">Add</button>
+    <form onSubmit={handleSubmit} className="form-container">
+      <div className="input-container">
+        <input
+          type="text"
+          value={todo}
+          onChange={handleChange}
+          placeholder="Enter your task"
+          className="form-input"
+        />
+        <button type="submit" className="form-button">
+          Add
+        </button>
+      </div>
     </form>
   );
 };
