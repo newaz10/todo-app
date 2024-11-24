7ccb0af484cecf599fc40c82c9c3df0fe8ff9aee
newaz10 <mh.newaz001@gmail.com>
2024-11-24 23:05:46 +0600

    adding a delete button

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -5,10 +5,16 @@ import { Form } from "./Form";
 export const Todo = () => {
   const [todoList, setTodoList] = useState([]); // State to hold the list of tasks
 
+  const deleteTodo = (indexToDelete) => {
+    setTodoList((prevList) =>
+      prevList.filter((_, index) => index !== indexToDelete)
+    );
+  };
+
   return (
     <div>
       <Form todoList={todoList} setTodoList={setTodoList} />
-      <TodoList todoList={todoList} />
+      <TodoList todoList={todoList} deleteTodo={deleteTodo} />
     </div>
   );
 };
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,19 +1,26 @@
 import PropTypes from "prop-types";
 import "./TodoList.css";
 
-export const TodoList = ({ todoList }) => {
+export const TodoList = ({ todoList, deleteTodo }) => {
   return (
     <ul className="todo-list">
-      {todoList.map((todoList, index) => (
+      {todoList.map((task, index) => (
         <li key={index} className="todo-item">
-          {todoList}
-        </li> // Render each task in the list
+          <span>{task}</span>
+          <button
+            className="delete-button"
+            onClick={() => deleteTodo(index)} // Call delete function with the index
+          >
+            X
+          </button>
+        </li>
       ))}
     </ul>
   );
 };
 
-// Add prop validation
+// Prop validation
 TodoList.propTypes = {
   todoList: PropTypes.arrayOf(PropTypes.string).isRequired, // tasks must be an array of strings and is required
+  deleteTodo: PropTypes.func.isRequired, // Ensure deleteTodo is a function
 };
